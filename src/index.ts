b29d98292ae72aad5e188d7cfa45d5092697ac90
DimaSalem <demamohamad1998@gmail.com>
2024-08-09 07:43:39 +0300

    basic server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
-console.log("Hello World...");
-console.log("Hello there");
-console.log("third trial..");
-console.log("third trial...");
+import express, { Application, Request, Response } from 'express'
 
+const PORT = 3001
+// create an instance server
+const app: Application = express()
+
+//add routing for / path
+// app.get('/', (req: Request, res: Response) => {
+//   res.json({
+//     message: 'Hello World 🌍',
+//   })
+// })
+
+//start express server
+app.listen(PORT, () => {
+  console.log(`Server is starting at port:${PORT}`)
+})
+export default app
